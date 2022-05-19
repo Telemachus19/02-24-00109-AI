091a230a746539fa70f1a94ebc82b920da4f2711
telemachus19 <ahmedashraf20212324@gmail.com>
2022-05-19 12:44:26 +0200

    final version again

--- Assignment2/ConnectFourGUI/src/main/java/com/connectfourgui/ConnectFourFactory.java
+++ Assignment2/ConnectFourGUI/src/main/java/com/connectfourgui/ConnectFourFactory.java
@@ -11,12 +11,12 @@ import javafx.scene.shape.Rectangle;
 
 
 public class ConnectFourFactory implements EntityFactory {
-    public final int OFFSET = 5;
+    private final int OFFSET = 5;
     @Spawns("Player")
     public Entity spawnsPlayer(SpawnData data){
         var TileSize = data.<Integer>get("Tile Size");
         var color = data.<Color>get("color");
-        var circle = new Circle(TileSize - (TileSize/2),TileSize - (TileSize/2),(TileSize/2) - OFFSET, color);
+        var circle = new Circle(TileSize - (TileSize/2.0),TileSize - (TileSize/2.0),(TileSize/2.0) - OFFSET, color);
         return FXGL.entityBuilder(data)
                 .type(EntityType.PLAYER)
                 .view(circle)
@@ -29,7 +29,7 @@ public class ConnectFourFactory implements EntityFactory {
         var color = data.<Color>get("color");
         var tileSize = data.<Integer>get("Tile Size");
         var rectangle = new Rectangle(tileSize,tileSize,Color.BLUE);
-        var circle = new Circle(tileSize - (tileSize/2), tileSize - (tileSize/2),(tileSize / 2) - OFFSET, color);
+        var circle = new Circle(tileSize - (tileSize/2.0), tileSize - (tileSize/2.0),(tileSize / 2.0) - OFFSET, color);
         return FXGL.entityBuilder(data)
                 .type(EntityType.TILE)
                 .view(rectangle)
